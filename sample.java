aef6e28e3fbd346e5f7987ca4c4b24527245cf18
Sunena-Sirajudeen <23203035@srcas.ac.in>
2024-07-01 12:22:07 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 class sample {
-public static void main(String[] args) {
-System. out. println("Welcome to devops class")
+public static void main(String[]args){
+System.out.println("Welcome to devops class");
 }
 }
